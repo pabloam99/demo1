8118f416901f7852ce0e249ea74e7bf599653f8b
pablo <pablo@PC-Pablo>
2022-02-14 14:42:54 +0100

    cambio prueba

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,12 +2,10 @@ package demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("provocar conflicto, SOLUCIONADO");
+		System.out.println("linea 1");
 		System.out.println("linea 2");
 		System.out.println("linea 3");
 		
-		//cambio desde rama cambio2
-		
-		//hacemos otro cambio desde cambio2
+	
 	}
 }
