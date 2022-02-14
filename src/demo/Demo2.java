e6d8b2f4438d00bb1afc6588eaa4e223b077e2db
pablo <pablo@PC-Pablo>
2022-02-14 14:46:25 +0100

    cambio en demo2

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -3,7 +3,11 @@ package demo;
 public class Demo2 {
 
 	public static void main(String[] args) {
+<<<<<<< Upstream, based on cambio3
 		//cambio en cambio3 para rebase
+=======
+		
+>>>>>>> 17a18ae cambio en demo2
 
 	}
 
